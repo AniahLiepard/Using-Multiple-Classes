5314546fb1a6c5d9c645f80c51f5a954af6b995d
AniahLiepard <aniah.garcia@techhigh.us>
2015-09-10 10:20:01 -0400

    Using Multiple Classes Project
    https://www.thenewboston.com/videos.php?cat=31&amp;video=17979

---
+++ ProjectEleven.java
@@ -0,0 +1,6 @@
+public class ProjectEleven {
+	public static void main(String args[]){
+		ProjectElevenPart2 item = new ProjectElevenPart2();
+		item.note();
+	}
+}
---
+++ ProjectElevenPart2.java
@@ -0,0 +1,5 @@
+public class ProjectElevenPart2 {
+	public void note(){
+		System.out.println("I'm eating SweetTarts right now, don't disturb me!");
+	}
+}
